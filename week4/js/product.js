5637f374f9a517437b65f23c2271120071550b36
hyxfish27 <avicky1515@gmail.com>
2022-01-29 18:52:27 +0800

    modal component

---
+++ week4/js/product.js
@@ -0,0 +1,22 @@
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.27/vue.esm-browser.min.js';
+import modal_product from './modal-product.js';
+
+const url = 'https://vue3-course-api.hexschool.io/v2';
+const path = 'hyxfish27';
+
+const app = {
+    data(){
+        return{
+
+        }
+    },
+    methods: {
+        
+    },
+    mounted() {
+        
+    },
+    component:{
+        modal_product
+    }
+}
