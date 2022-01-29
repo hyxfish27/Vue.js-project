eb7c272e256c18dd7c1bc8967548cb8b21543e1c
hyxfish27 <avicky1515@gmail.com>
2022-01-29 19:04:09 +0800

    week3 進度

--- week4/js/product.js
+++ week4/js/product.js
@@ -4,19 +4,39 @@ import modal_product from './modal-product.js';
 const url = 'https://vue3-course-api.hexschool.io/v2';
 const path = 'hyxfish27';
 
+// 取得Token
+const token = document.cookie.replace(/(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+axios.defaults.headers.common['Authorization'] = token;
+
 const app = {
-    data(){
-        return{
+    data() {
+        return {
 
         }
     },
     methods: {
-        
+        // Get product
+        getProduct() {
+
+        },
+        // check Admin
+        checkAdmin() {
+            axios
+                .post(`${url}/api/user/check`)
+                .then(res => {
+                    // console.log(res);
+                    this.getProducts();
+                })
+                .catch(err => {
+                    alert(err.data.message);
+                    window.location = "index.html"
+                })
+        }
     },
     mounted() {
-        
+        this.checkAdmin();
     },
-    component:{
+    component: {
         modal_product
     }
 }
