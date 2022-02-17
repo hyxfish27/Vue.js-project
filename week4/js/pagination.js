7212ed81551ccc5c103d80518b31e7ceba18edee
hyxfish27 <avicky1515@gmail.com>
2022-02-17 16:05:52 +0800

    modals finish

--- week4/js/pagination.js
+++ week4/js/pagination.js
@@ -1,8 +1,9 @@
 export default {
-    props: ['pages'],
-    template: `<nav aria-label="Page navigation example">
+  props: ['pages'],
+  template: `<nav aria-label="Page navigation example">
     <ul class="pagination">
-      <li class="page-item" :class = "{ disabled: pages.has_pre }">
+      <li class="page-item" :class = "{ disabled: !pages.has_pre }"
+      @click="$emit('get-product', pages.current_page - 1)">
         <a class="page-link" href="#" aria-label="Previous">
           <span aria-hidden="true">&laquo;</span>
         </a>
@@ -10,9 +11,10 @@ export default {
       <li class="page-item" 
       :class = "{ active: page === pages.current_page }"
       v-for="page in pages.total_pages" :key="'page' + page"><a class="page-link" href="#"
-      @click=$emit('get-product', page)>{{page}}</a>
+      @click="$emit('get-product', page)">{{page}}</a>
       </li>
-      <li class="page-item" :class = "{ disabled: !pages.has_next }">
+      <li class="page-item" :class = "{ disabled: !pages.has_next }"
+      @click="$emit('get-product', pages.current_page + 1)">
         <a class="page-link" href="#" aria-label="Next">
           <span aria-hidden="true">&raquo;</span>
         </a>
